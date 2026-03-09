edd84cebdd9b37547b413c48edf309ec84d2780c
NakkacheSalma <salma.nakkache.etu23@ensem.ac.ma>
2026-03-09 09:42:28 +0000

    Add Readme File

--- dbt_project/macros/test_cancelled_status_discount_check.sql
+++ dbt_project/macros/test_cancelled_status_discount_check.sql
@@ -1,3 +1,3 @@
 {% test cancelled_status_discount_check(model) %}
-SELECT * FROM {{ model }} WHERE status = 'cancelled' AND TRY_CAST(discount_pct AS FLOAT) <> 0
+SELECT * FROM {{ model }} WHERE status = 'cancelled' AND TRY_CAST(discount_pct AS FLOAT) != 0
 {% endtest %}
