cc66411dc924bd00dc043cd46fb65b2cf79aefa5
seddib03 <s.eddibsalma@gmail.com>
2026-03-13 11:38:15 +0000

    update .env nlq

--- data_preparation_agent/dbt_project/macros/generated/test_delay_minutes_non_negative.sql
+++
@@ -1,3 +0,0 @@
-{% test delay_minutes_non_negative(model, column_name) %}
-SELECT * FROM {{ model }} WHERE TRY_CAST(delay_minutes AS FLOAT) < 0
-{% endtest %}
--- data_preparation_agent/dbt_project/macros/generated/test_satisfaction_score_within_limit.sql
+++
@@ -1,3 +0,0 @@
-{% test satisfaction_score_within_limit(model, column_name) %}
-SELECT * FROM {{ model }} WHERE TRY_CAST(satisfaction_score AS FLOAT) > 5
-{% endtest %}
