f010e5de61bf494e9563a0ad8e45e75c10434ca5
NakkacheSalma <salma.nakkache.etu23@ensem.ac.ma>
2026-03-18 11:39:51 +0000

    delete data prep folder

--- data_preparation_agent/dbt_project/macros/test_not_null_string.sql
+++
@@ -1,5 +0,0 @@
-{% test not_null_string(model, column_name) %}
-SELECT * FROM {{ model }}
-WHERE UPPER(TRIM(CAST({{ column_name }} AS VARCHAR)))
-      IN ('NULL', 'N/A', 'NONE', 'NA', 'NAN', '-', '?', 'MISSING', 'UNKNOWN')
-{% endtest %}
